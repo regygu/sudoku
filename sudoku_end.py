6f688dde226e44d063af18bc3290321d8c85fbcd
Gergely Ugyan <ugyan.gergely@gmail.com>
2016-10-27 11:12:00 +0200

    Add files via upload

---
+++ sudoku_end.py
@@ -0,0 +1,214 @@
+map_easy = [[9, 8, " ", 2, 4, " ", " ", 5, 3],
+[6, " ", " ", 7, 3, 8, 1, 2, " "],
+[3, " ", 1, " ", 9, 5, " ", 8, 4],
+[8, 4, 3, 5, 1, " ", " ", " ", 6],
+[7, " ", " ", 4, 2, " ", 8, 3, " "],
+[2, 1, " ", 8, 7, 3, 4, " ", 5],
+[1, " ", 8, " ", 6, " ", 5, " ", 2],
+[" ", 6, 2, 3, 8, 4, " ", 1, 7],
+[4, " ", " ", 1, 5, 2, 3, " ", 8]]
+
+map_easy_full = [[9, 8, "\033[34m" + "7" + "\033[0m", 2, 4, "\033[34m" + "1" + "\033[0m", "\033[34m" + "6" + "\033[0m", 5, 3],
+[6, "\033[34m" + "5" + "\033[0m", "\033[34m" + "4" + "\033[0m", 7, 3, 8, 1, 2, "\033[34m" + "9" + "\033[0m"],
+[3, "\033[34m" + "2" + "\033[0m", 1, "\033[34m" + "6" + "\033[0m", 9, 5, "\033[34m" + "7" + "\033[0m", 8, 4],
+[8, 4, 3, 5, 1, "\033[34m" + "9" + "\033[0m", "\033[34m" + "2" + "\033[0m", "\033[34m" + "7" + "\033[0m", 6],
+[7, "\033[34m" + "9" + "\033[0m", "\033[34m" + "5" + "\033[0m", 4, 2, "\033[34m" + "6" + "\033[0m", 8, 3, "\033[34m" + "1" + "\033[0m"],
+[2, 1, "\033[34m" + "6" + "\033[0m", 8, 7, 3, 4, "\033[34m" + "9" + "\033[0m", 5],
+[1, "\033[34m" + "3" + "\033[0m", 8, "\033[34m" + "9" + "\033[0m", 6, "\033[34m" + "7" + "\033[0m", 5, "\033[34m" + "4" + "\033[0m", 2],
+["\033[34m" + "5" + "\033[0m", 6, 2, 3, 8, 4, "\033[34m" + "9" + "\033[0m", 1, 7],
+[4, "\033[34m" + "7" + "\033[0m", "\033[34m" + "9" + "\033[0m", 1, 5, 2, 3, "\033[34m" + "6" + "\033[0m", 8]]
+
+map_medium = [[" ", 8, " ", 2, 4, " ", " ", 5, 3],
+[6, " ", " ", 7, " ", 8, " ", 2, " "],
+[3, " ", " ", " ", 9, 5, " ", 8, 4],
+[8, 4, 3, " ", 1, " ", " ", " ", 6],
+[7, " ", " ", 4, 2, " ", " ", 3, " "],
+[" ", 1, " ", 8, 7, 3, 4, " ", 5],
+[1, " ", 8, " ", 6, " ", 5, " ", 2],
+[" ", 6, " ", 3, 8, 4, " ", 1, " "],
+[4, " ", " ", " ", 5, 2, 3, " ", 8]]
+
+map_medium_full = [["\033[34m" + "9" + "\033[0m", 8, "\033[34m" + "7" + "\033[0m", 2, 4, "\033[34m" + "1" + "\033[0m", "\033[34m" + "6" + "\033[0m", 5, 3],
+[6, "\033[34m" + "5" + "\033[0m", "\033[34m" + "4" + "\033[0m", 7, "\033[34m" + "3" + "\033[0m", 8, "\033[34m" + "1" + "\033[0m", 2, "\033[34m" + "9" + "\033[0m"],
+[3, "\033[34m" + "2" + "\033[0m", "\033[34m" + "1" + "\033[0m", "\033[34m" + "6" + "\033[0m", 9, 5, "\033[34m" + "7" + "\033[0m", 8, 4],
+[8, 4, 3, "\033[34m" + "5" + "\033[0m", 1, "\033[34m" + "9" + "\033[0m", "\033[34m" + "2" + "\033[0m", "\033[34m" + "7" + "\033[0m", 6],
+[7, "\033[34m" + "9" + "\033[0m", "\033[34m" + "5" + "\033[0m", 4, 2, "\033[34m" + "6" + "\033[0m", "\033[34m" + "8" + "\033[0m", 3, "\033[34m" + "1" + "\033[0m"],
+["\033[34m" + "2" + "\033[0m", 1, "\033[34m" + "6" + "\033[0m", 8, 7, 3, 4, "\033[34m" + "9" + "\033[0m", 5],
+[1, "\033[34m" + "3" + "\033[0m", 8, "\033[34m" + "9" + "\033[0m", 6, "\033[34m" + "7" + "\033[0m", 5, "\033[34m" + "4" + "\033[0m", 2],
+["\033[34m" + "5" + "\033[0m", 6, "\033[34m" + "2" + "\033[0m", 3, 8, 4, "\033[34m" + "9" + "\033[0m", 1, "\033[34m" + "7" + "\033[0m"],
+[4, "\033[34m" + "7" + "\033[0m", "\033[34m" + "9" + "\033[0m", "\033[34m" + "1" + "\033[0m", 5, 2, 3, "\033[34m" + "6" + "\033[0m", 8]]
+
+map_hard = [[" ", " ", " ", 2, 4, " ", " ", " ", 3],
+[6, " ", " ", 7, " ", 8, " ", 2, " "],
+[" ", " ", " ", " ", 9, 5, " ", 8, 4],
+[8, 4, 3, " ", 1, " ", " ", " ", 6],
+[7, " ", " ", " ", 2, " ", " ", 3, " "],
+[" ", 1, " ", 8, 7, 3, 4, " ", 5],
+[1, " ", 8, " ", " ", " ", 5, " ", 2],
+[" ", 6, " ", 3, 8, 4, " ", 1, " "],
+[4, " ", " ", " ", 5, " ", 3, " ", 8]]
+
+map_hard_full = [["\033[34m" + "9" + "\033[0m", "\033[34m" + "8" + "\033[0m", "\033[34m" + "7" + "\033[0m", 2, 4, "\033[34m" + "1" + "\033[0m", "\033[34m" + "6" + "\033[0m", "\033[34m" + "5" + "\033[0m", 3],
+[6, "\033[34m" + "5" + "\033[0m", "\033[34m" + "4" + "\033[0m", 7, "\033[34m" + "3" + "\033[0m", 8, "\033[34m" + "1" + "\033[0m", 2, "\033[34m" + "9" + "\033[0m"],
+["\033[34m" + "3" + "\033[0m", "\033[34m" + "2" + "\033[0m", "\033[34m" + "1" + "\033[0m", "\033[34m" + "6" + "\033[0m", 9, 5, "\033[34m" + "7" + "\033[0m", 8, 4],
+[8, 4, 3, "\033[34m" + "5" + "\033[0m", 1, "\033[34m" + "9" + "\033[0m", "\033[34m" + "2" + "\033[0m", "\033[34m" + "7" + "\033[0m", 6],
+[7, "\033[34m" + "9" + "\033[0m", "\033[34m" + "5" + "\033[0m", "\033[34m" + "4" + "\033[0m", 2, "\033[34m" + "6" + "\033[0m", "\033[34m" + "8" + "\033[0m", 3, "\033[34m" + "1" + "\033[0m"],
+["\033[34m" + "2" + "\033[0m", 1, "\033[34m" + "6" + "\033[0m", 8, 7, 3, 4, "\033[34m" + "9" + "\033[0m", 5],
+[1, "\033[34m" + "3" + "\033[0m", 8, "\033[34m" + "9" + "\033[0m", "\033[34m" + "6" + "\033[0m", "\033[34m" + "7" + "\033[0m", 5, "\033[34m" + "4" + "\033[0m", 2],
+["\033[34m" + "5" + "\033[0m", 6, "\033[34m" + "2" + "\033[0m", 3, 8, 4, "\033[34m" + "9" + "\033[0m", 1, "\033[34m" + "7" + "\033[0m"],
+[4, "\033[34m" + "7" + "\033[0m", "\033[34m" + "9" + "\033[0m", "\033[34m" + "1" + "\033[0m", 5, "\033[34m" + "2" + "\033[0m", 3, "\033[34m" + "6" + "\033[0m", 8]]
+
+def print_map():
+    print(map[0][0], map[0][1], map[0][2], "|", map[0][3], map[0][4], map[0][5], "|", map[0][6], map[0][7], map[0][8])
+    print(map[1][0], map[1][1], map[1][2], "|", map[1][3], map[1][4], map[1][5], "|", map[1][6], map[1][7], map[1][8])
+    print(map[2][0], map[2][1], map[2][2], "|", map[2][3], map[2][4], map[2][5], "|", map[2][6], map[2][7], map[2][8])
+    print("-------" *3)
+    print(map[3][0], map[3][1], map[3][2], "|", map[3][3], map[3][4], map[3][5], "|", map[3][6], map[3][7], map[3][8])
+    print(map[4][0], map[4][1], map[4][2], "|", map[4][3], map[4][4], map[4][5], "|", map[4][6], map[4][7], map[4][8])
+    print(map[5][0], map[5][1], map[5][2], "|", map[5][3], map[5][4], map[5][5], "|", map[5][6], map[5][7], map[5][8])
+    print("-------" *3)
+    print(map[6][0], map[6][1], map[6][2], "|", map[6][3], map[6][4], map[6][5], "|", map[6][6], map[6][7], map[6][8])
+    print(map[7][0], map[7][1], map[7][2], "|", map[7][3], map[7][4], map[7][5], "|", map[7][6], map[7][7], map[7][8])
+    print(map[8][0], map[8][1], map[8][2], "|", map[8][3], map[8][4], map[8][5], "|", map[8][6], map[8][7], map[8][8])
+
+diff = input("Válasszon nehézségi szintet: Könnyű [1] / Közepes [2] / Nehéz [3] ")
+if diff == "1":
+   map = map_easy
+       
+elif diff == "2":
+   map = map_medium
+       
+elif diff == "3":
+   map = map_hard
+else:
+   exit()
+
+print()
+print_map()
+
+def main():
+    print()
+    list_one_ten = range(1,10)
+    choose = input ("Válasszon: I = Bevitel / D = Törlés / C = Ellenőrzés / R = Játékszabály /E = Kilépés -> ")
+    print()
+
+    if choose == "I":
+        while True:
+            try:
+                a = int(input("Írja be az oszlop számát(1-9): "))
+                if a in list_one_ten:
+                    a = a - 1
+                    break
+                else:
+                    print("Ez nem a megadott intervallumban található szám!")
+                    print("")
+                    print_map()
+                    main()
+            except ValueError:
+                print("Ez nem egy szám!")
+                print("")
+                print_map()
+                main()
+        while True:
+            try:
+                b = int(input("Írja be a sor számát(1-9): "))
+                if b in list_one_ten:
+                    b = b - 1
+                    break
+                else:
+                    print("Ez nem a megadott intervallumban található szám!")
+                    print("")
+                    print_map()
+                    main()
+            except ValueError:
+                print("Ez nem egy szám!")
+                print("")
+                print_map()
+                main()
+        while True:
+            try:
+                c = int (input("Írja be a behelyettesíteni kívánt számot(1-9): "))
+                if c in list_one_ten:
+                    break
+                else:
+                    print("Ez nem a megadott intervallumban található szám!")
+                    print("")
+                    print_map()
+                    main()
+            except ValueError:
+                print("Ez nem egy szám!")
+                print("")
+                print_map()
+                main()
+        d = str(c)
+        if map [b][a] == " ":
+            map[b][a] = "\033[34m" + d + "\033[0m"
+        else:
+            print ("Ez a szám nem módosítható!")
+    
+    elif choose == "D":
+        while True:
+            try:
+                a = int(input("Írja be az oszlop számát(1-9): "))
+                if a in list_one_ten:
+                    a = a - 1
+                    break
+                else:
+                    print("Ez nem a megadott intervallumban található szám!")
+                    print("")
+                    print_map()
+                    main()
+            except ValueError:
+                print("Ez nem egy szám!")
+                print("")
+                print_map()
+                main()
+        while True:
+            try:
+                b = int(input("Írja be a sor számát(1-9): "))
+                if b in list_one_ten:
+                    b = b - 1
+                    break
+                else:
+                    print("Ez nem a megadott intervallumban található szám!")
+                    print("")
+                    print_map()
+                    main()
+            except ValueError:
+                print("Ez nem egy szám!")
+                print("")
+                print_map()
+                main()
+        
+        if map [b][a] == " ":
+            print ("Innen nincs mit törölni!")
+        elif isinstance(map [b][a], str):
+            map [b][a] = " "
+        else:
+            print("Ez a szám nem törölhető!")
+
+    elif choose == "R":
+        print(25* " " + "JÁTÉKSZABÁLY \n A 9x9-es tábla összes sorába és oszlopába írd be a számokat 1-9-ig úgy, \n hogy minden szám csak egyszer szerepelhet soronként vagy oszloponként. \n A nagy négyzetrács további 3x3-as négyzeteiben is csak egyszer szerepelhet egy szám.")
+
+    elif choose == "C":
+        if map == map_easy_full:
+            print ("Gratulálok, nyertél!")
+            exit()
+        elif map == map_medium_full:
+            print ("Gratulálok, nyertél!")
+            exit()
+        elif map == map_hard_full:
+            print ("Gratulálok, nyertél!")
+            exit()
+        else:
+            print("Valami még biztosan hibás, vagy hiányzik. Ellenőrizd és javítsd ki a hibát!") 
+        
+    elif choose == "E":
+        exit()
+    else:
+        print("Ez a karakter nem szerepel a lehetőségek között. Próbáld újra!")
+    print()
+    print_map()
+    print()
+    main()
+main()
